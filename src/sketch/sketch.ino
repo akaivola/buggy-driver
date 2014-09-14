d41c0a2a260eee7eb3dc194aa35b59677eb9f92b
Aki Kaivola <aki.kaivola@gmail.com>
2014-09-14 19:51:39 +0300

    Configure pins for final project

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -1,5 +1,5 @@
-int motor_A=12;
-int motor_B=13;
+int motor_A=5;
+int motor_B=6;
 int motor_Speed=3;
 int switchIn=0;
 int currentSpeed=0;
@@ -11,9 +11,12 @@ int reverseStickPin=5;
 int reverseOrForward=1;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(motor_A, OUTPUT);
   pinMode(motor_B, OUTPUT);
+  pinMode(motor_Speed, OUTPUT);
+  pinMode(forwardStickPin, INPUT);
+  pinMode(reverseStickPin, INPUT);
+  pinMode(switchIn, INPUT);
   digitalWrite(motor_A, HIGH);
   digitalWrite(motor_B, LOW);
 }
@@ -30,11 +33,11 @@ int readShiftStick() {
   int f = !analogRead(forwardStickPin);
   int r = !analogRead(reverseStickPin);
   bool bothOff = !(f | r);
-  Serial.print(f);
-  Serial.print("/");
-  Serial.print(r);
-  Serial.print(" , ");
-  Serial.print(bothOff);
+  //Serial.print(f);
+  //Serial.print("/");
+  //Serial.print(r);
+  //Serial.print(" , ");
+  //Serial.print(bothOff);
 
   int newReverseOrForward = bothOff ? reverseOrForward : f ? 1 : 0;
   return reverseOrForward ^ newReverseOrForward  && currentSpeed == 0 ? newReverseOrForward : reverseOrForward;
@@ -48,7 +51,7 @@ void loop() {
   digitalWrite(motor_B, reverseOrForward ? LOW : HIGH);
 
   currentSpeed = setSpeed(powerSwitch ? acceleration : brakeSpeed);
-  Serial.println(reverseOrForward);
+  //Serial.println(reverseOrForward);
 
   delay(currentSpeed == 0 ? 300 : 10);
 }
